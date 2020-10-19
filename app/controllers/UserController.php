10b125b3e36f9de15ae08dc203b6303f35e380af
exezio <dmitryzlo111@gmail.com>
2020-10-19 16:53:09 +0300

    добавлено хеширование паролей при записи юзера в БД

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -22,6 +22,7 @@ class UserController extends Controller
             $user->getErrorsSignup();
             redirect();
         }
+        $user->attributesSignup['password'] = password_hash($user->attributesSignup['password'], PASSWORD_DEFAULT);
         if($user->saveAttributesSignup())
         {
             $_SESSION['success'] = 'Вы успешно зарегестрированны';
